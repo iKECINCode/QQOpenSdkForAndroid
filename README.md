114b5115982763de9d92e720cd5a70115ecf911f
yangheng <yangheng_work@live.com>
2026-07-09 17:10:56 +0800

    同步3.5.19

--- README.md
+++ README.md
@@ -4,6 +4,4 @@
 
 支持jitpack
 
-http://wiki.open.qq.com/wiki/mobile/SDK%E4%B8%8B%E8%BD%BD
-
 https://wiki.connect.qq.com/sdk%E4%B8%8B%E8%BD%BD
--- app/src/main/java/com/ikecin/demo/MainActivity.java
+++ app/src/main/java/com/ikecin/demo/MainActivity.java
@@ -1,25 +1,25 @@
 package com.ikecin.demo;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.example.tencentConnect.R;
+import com.ikecin.demo.databinding.ActivityMainBinding;
 import com.tencent.tauth.Tencent;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        findViewById(R.id.ok).setOnClickListener(v -> {
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.ok.setOnClickListener(v -> {
             Tencent mTencent = Tencent.createInstance("1104677783", getApplicationContext());
             Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_SHORT).show();
         });
     }
-
 }
